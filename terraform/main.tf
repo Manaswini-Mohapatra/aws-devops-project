b1d5ce242b82b526cc826f4fe87b8825e58c77ff
Manaswini-Mohapatra <manaswinimohapatra1234@gmail.com>
2025-07-26 22:13:12 +0530

    fix: added S3 encryption to pass tfsec

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,14 @@ provider "aws" {
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket        = var.bucket_name
   force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
